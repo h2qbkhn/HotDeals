a051a0d49c53f58a2afda29f606fa67085ca55bb
Hong Quan <hqho@mcnext.com>
2016-08-08 12:02:35 +0200

    fin tabs user

---
+++ HotDeals.Website/app/controllers/user/user-controller.ts
@@ -0,0 +1,42 @@
+﻿/// <reference path="../../../Scripts/typings/angularjs/angular.d.ts"/>
+/// <reference path="../../../Scripts/typings/angularjs/angular-resource.d.ts"/>
+module HQHO.HotDeals {
+    "use strict";
+    export interface IUserViewModel {
+        menus: any[]; 
+        selectedMenu: any; 
+    }
+   
+    export interface IUserScopeMethod {
+      
+    }
+    export interface IUserScope extends ng.IScope {
+        vm: IUserViewModel;
+        mt: IUserScopeMethod;
+    }
+
+    export class UserController {
+        constructor(private $scope: IUserScope,
+            private $rootScope: IHotDealsRootScope
+            ) {
+            this.$scope.mt = {
+                
+            }
+
+            this.$scope.vm = {
+                menus : [
+                    {name: 'Overview', icon:'glyphicon-home'}, 
+                    {name: 'Settings', icon:'glyphicon-user'}, 
+                    { name: 'Tasks', icon: 'glyphicon-ok' }, 
+                    { name: 'Help', icon: 'glyphicon-flag' }, 
+                ], 
+                selectedMenu : 0
+            }
+
+
+        }
+    
+    }
+
+    angular.module('HotDeals').controller('UserCtrl', ['$scope', '$rootScope', UserController]);
+}
---
+++ HotDeals.Website/app/services/user/userService.ts
@@ -0,0 +1,21 @@
+﻿module HQHO.HotDeals.Services {
+    "use strict";
+    export interface IUserService {
+
+    }
+    export class UserService extends Services.BaseService implements IUserService {
+        constructor($http: ng.IHttpService) {
+            super($http, "users");
+        }
+        public getUserById(userId: string): ng.IHttpPromise<any> {
+            return this.$http.get(Tools.apiServiceBaseUrl + this.name, {
+                params: {
+                    userId: userId
+                }
+
+            })
+        }
+    }
+
+    angular.module('HotDeals').service('UserServ', UserService);
+}
